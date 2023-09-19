c336bf22694f92dde79406e3a62c9e88a384b0ec
loco9939 <kls9939@naver.com>
2023-09-19 14:41:40 +0900

    feat: 이미지 자료 배럴화

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import arrowRight from "./arrow-right.svg";
+import close from "./close.svg";
+import level1 from "./level1.svg";
+import level2 from "./level2.svg";
+import level3 from "./level3.svg";
+
+export { arrowRight, close, level1, level2, level3 };
