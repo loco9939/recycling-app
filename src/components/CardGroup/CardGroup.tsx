ead222e77be5db08a5e9c2ebfaed5b70a28581f6
loco9939 <kls9939@naver.com>
2023-09-19 14:39:12 +0900

    feat: 카드 컴포넌트 생성

---
+++ src/components/CardGroup/CardGroup.tsx
@@ -0,0 +1,47 @@
+import { level1, level2, level3 } from "@/assets";
+import Card from "@/modules/Card/Card";
+import React from "react";
+import style from "./CardGroup.module.css";
+
+const cards = [
+  {
+    level: 1,
+    imgUrl: level1,
+    title: "새싹 1단계",
+    content: "분리수거에 관심을 이제 막 가지기 시작한 새싹 단계",
+    tags: ["새싹", "분린이"],
+  },
+  {
+    level: 2,
+    imgUrl: level2,
+    title: "가지 2단계",
+    content: "분리수거에 어느정도 일가견이 있으신 가지 단계",
+    tags: ["가지", "분자감"],
+  },
+  {
+    level: 3,
+    imgUrl: level3,
+    title: "나무 3단계",
+    content: "주변 사람에게 분리수거 방법을 알려주는 나무 단계",
+    tags: ["나무", "분리왕"],
+  },
+];
+
+function CardGroup() {
+  return (
+    <div className={style["card-group"]}>
+      {cards.map(({ level, imgUrl, title, content, tags }) => (
+        <Card
+          key={title}
+          level={level}
+          imgUrl={imgUrl}
+          title={title}
+          content={content}
+          tags={tags}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default CardGroup;
---
+++ src/modules/Card/Card.tsx
@@ -0,0 +1,34 @@
+import { level1 } from "@/assets";
+import Image from "next/image";
+import React from "react";
+import style from "./Card.module.css";
+
+interface Props {
+  level: number;
+  imgUrl: string;
+  title: string;
+  content: string;
+  tags: string[];
+}
+function Card({ level, imgUrl, title, content, tags }: Props) {
+  return (
+    <div className={style.card}>
+      <Image
+        src={imgUrl}
+        alt={title}
+        width={40}
+        height={40}
+        className={style.img}
+      />
+      <h2 className={style.title}>{title}</h2>
+      <p className={style.content}>{content}</p>
+      <div className={style["tag-box"]}>
+        {tags.map((tag) => (
+          <span key={tag}>#{tag}</span>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Card;
