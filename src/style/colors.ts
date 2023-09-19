4577f54a904f9ed5cd7c350cc1dbd4412e0347a2
loco9939 <kls9939@naver.com>
2023-09-19 14:39:44 +0900

    feat: 전역 스타일 추가 및 색상 스타일 추가

---
+++ src/style/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  Primary: "#1CB258",
+  Black: "#1F1300",
+  Black_Secondary: "#3F3F3F",
+  White: "#ffffff",
+  Red: "#CC444B",
+  Yellow: "#F4F90F",
+  Gray: "#9F9F9F",
+  Gray_Secondary: "#CFCFCF",
+};
