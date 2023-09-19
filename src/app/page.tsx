8bf113d02f6da2fd8b60f4c579b69c6800a5e71e
loco9939 <kls9939@naver.com>
2023-09-19 14:41:14 +0900

    feat: 메인 페이지 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,37 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Button from "@/modules/Button/Button";
+import Header from "@/components/Header/Header";
+import { level1 } from "@/assets";
+import ScrollAnimation from "@/components/ScrollAnimation/ScrollAnimation";
+import CardGroup from "@/components/CardGroup/CardGroup";
+import Footer from "@/components/Footer/Footer";
 
 export default function Home() {
   return (
-    <div className={styles['main-layout']}>
-      <header>분리수거 App</header>
-      <main>
-        <section>
-          <h1>분리수거 App</h1>
-          <p>까다로운 분리수거 방법 때문에 늘 고민이 많으셨죠?</p>
-          <p>
-            재밌게 즐길 수 있는 간단한 퀴즈로 나의 분리수거 Level을 체크해보세요
+    <div className={styles["main-layout"]}>
+      <Header />
+      <main style={{ textAlign: "center" }}>
+        <section className={styles.desc}>
+          <h1 className={styles["desc-title"]}>분리수거 App</h1>
+          <p className={styles["desc-content"]} style={{ margin: "38px" }}>
+            까다로운 분리수거 방법 때문에 늘 고민이 많으셨죠?
+          </p>
+          <p className={styles["desc-content"]}>
+            재밌게 즐길 수 있는 간단한 퀴즈로 <br />
+            나의 분리수거 Level을 체크해보세요
           </p>
         </section>
 
-        <section>scroll Animations</section>
+        <ScrollAnimation />
 
-        <section>
-          <div>
-            <img src="" alt="" />
-            <h2>새싹 1단계</h2>
-            <p>분리수거에 관심을 이제 막 가지기 시작한 새싹 단계</p>
-            <div>
-              <span>#새싹</span>
-              <span>#분린이</span>
-            </div>
-          </div>
-        </section>
+        <div className={styles["card-group-container"]}>
+          <CardGroup />
+        </div>
 
-        <button>시작하기</button>
+        <Button className={styles.start}>시작하기</Button>
       </main>
-      <footer className="border">푸터</footer>
+      <Footer />
     </div>
   );
 }
